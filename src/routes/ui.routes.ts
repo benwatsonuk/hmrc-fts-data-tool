10cdebfc42b30b16d1541010780a47d121f203b6
benwatsonuk <hello@benwatson.eu>
2026-05-13 16:32:01 +0100

    UI getting there

--- src/routes/ui.routes.ts
+++ src/routes/ui.routes.ts
@@ -4,13 +4,24 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get(
-  "/",
-  (_req, res) => {
-    res.render("ui/index.html", {
-      title: "Domain Intelligence API - UI"
-    });
-  }
-);
+router.get("/", (_req, res) => {
+  res.render("ui/index.html", {
+    title: "Domain Intelligence API - UI"
+  });
+});
+
+router.post("/", (_req, res) => {
+  res.render("ui/index.html", {
+    title: "Domain Intelligence API - UI"
+  });
+});
+
+router.get("/:id", (_req, res) => {
+  const domainInfo = { title: "hello" }
+  res.render("ui/single.html", {
+    title: "Domain Intelligence API - UI",
+    domainInfo: domainInfo
+  });
+});
 
 export default router;
